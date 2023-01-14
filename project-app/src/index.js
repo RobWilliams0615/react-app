e98edc74a7c290bb91a88239e57aff80b51b950b
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-14 15:53:26 -0700

    practice reminder of jsx and react structure

--- project-app/src/index.js
+++ project-app/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// React cannot show an object as text content
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
 
 function App() {
-  return <h1>Rob Project</h1>;
+  const greeting = 'Welcome to my site!';
+  const name = 'Rob';
+  const age = 45;
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <h1>
+        My name is {name} and my age is {age}
+      </h1>
+    </div>
+  );
 }
 
 root.render(<App />);
