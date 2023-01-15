e62a31c9d9eb65e136a4db1084e76d31d20e0680
Rob Williams <92275209+RobWilliams0615@users.noreply.github.com>
2023-01-15 13:31:28 -0700

    updated columns to is-3 to make images smaller

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -12,21 +12,21 @@ function App() {
       <div className="container">
         <section className="section">
           <div className="columns">
-            <div className="column is-4">
+            <div className="column is-3">
               <ProfileCard
                 title="Alexa"
                 username="@alexa99"
                 image={AlexaImage}
               />
             </div>
-            <div className="column is-4">
+            <div className="column is-3">
               <ProfileCard
                 title="Cortana"
                 username="@cortana23"
                 image={CortanaImage}
               />
             </div>
-            <div className="column is-4">
+            <div className="column is-3">
               <ProfileCard title="Siri" username="@siri10" image={SiriImage} />
             </div>
           </div>
